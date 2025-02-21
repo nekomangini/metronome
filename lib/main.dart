0d41bd3b419e2e8e0fca456b1b8bc040ea730682
nekomangini <encar.luis.salazar@gmail.com>
2025-02-21 22:22:55 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MetronomeApp());
+}
+
+class MetronomeApp extends StatelessWidget {
+  const MetronomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Placeholder());
+  }
+}
